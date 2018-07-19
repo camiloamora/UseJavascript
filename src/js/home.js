f3ad0b41e04f0942e255bfb181ada976e211cd44
CamiloMora <kalufau@hotmail.com>
2018-07-18 20:50:05 -0500

    Promesas then cath, race

--- src/js/home.js
+++ src/js/home.js
@@ -6,3 +6,49 @@ let cambia = "@LeonidasEsteban"
 function cambiarNombre(nuevoNombre) {
   cambia = nuevoNombre
 }
+
+const getUserAll = new Promise(function(todoBien , todoMal){
+  //Llamar a un api
+  setTimeout(function (){
+    //luego de 3 segundos
+    todoMal('se acabo el tiempo');
+  }, 5000)
+})
+
+const getUser = new Promise(function(todoBien , todoMal){
+  //Llamar a un api
+  setTimeout(function (){
+    //luego de 3 segundos
+    todoBien('se acabo el tiempo 3');
+  }, 3000)
+})
+
+// getUser
+//   .then(function(){
+//     console.log('Todo esta bien')
+//   })
+//   .catch(function(er){
+//     console.log('Todo mal ${er}' );
+//   })
+
+// Promise.all([
+//   getUserAll,
+//   getUser
+// ])
+// .then(function(msg){
+//   console.log(msg)
+// })
+// .catch(function(er){
+//   console.log(er);
+// })
+
+Promise.race([
+  getUserAll,
+  getUser
+])
+.then(function(msg){
+  console.log(msg)
+})
+.catch(function(er){
+  console.log(er);
+})
