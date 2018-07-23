b26588dc2db49377a9ad8f95d23449fd2237184c
CamiloMora <kalufau@hotmail.com>
2018-07-22 21:02:16 -0500

    Refactorización de código para la carga de lista de acción, drama, animación

--- src/js/home.js
+++ src/js/home.js
@@ -120,21 +120,33 @@ fetch('https://randomuser.me/api/')
       )
     }
     // console.log(videoItemTemplate('src/imges/covers/bitcoin.jpg', 'bitcoin'));
-
-    const $actionContainer = document.querySelector('#action')
-
-    actionList.data.movies.forEach((movie) => {
-      const HTMLString = videoItemTemplate(movie);
+    function createTemplate(HTMLString){
       const html = document.implementation.createHTMLDocument();
       html.body.innerHTML = HTMLString
-      // debugger
-      console.log(HTMLString);
-      $actionContainer.append(html.body.children[0])
-    })
-
+      return html.body.children[0]
+    }
     
+
+    const $actionContainer = document.querySelector('#action')
+    function renderMovieList (list, $container) {
+      // actionList.data.movies
+      $container.children[0].remove();
+      list.forEach((movie) => {
+        const HTMLString = videoItemTemplate(movie);
+        const movieElement = createTemplate(HTMLString)
+        // debugger
+        console.log(HTMLString);
+        $container.append(movieElement)
+      })
+    }
+
     const $dramaContainer = document.querySelector('#drama')
     const $animationContainer = document.querySelector('#animation')
+
+    renderMovieList(actionList.data.movies, $actionContainer);
+    renderMovieList(dramaList.data.movies, $dramaContainer);
+    renderMovieList(animationList.data.movies, $animationContainer);
+
 
     const $featuringContainer = document.querySelector('#featuring')
     const $form = document.querySelector('#form')
